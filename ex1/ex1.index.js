5dc6f2afa6d661b995f49f58644429f0a21f8952
DANIEL886886 <danielparaschiv886@gmail.com>
2024-05-09 12:30:44 +0300

    Ex1_Sprint7-JS_finalizat

---
+++ ex1/ex1.index.js
@@ -0,0 +1,11 @@
+const n = 10;
+console.log ("Urmeaza sa fie expuse in ordine crescatoare numerele pare :")
+for (let i = 0; i <= n; i += 2) {
+  console.log(i);
+}
+
+console.log ("Urmeaza sa fie expuse in ordine descrescatoare numerele pare :")
+for (let i = n; i >= 0; i -= 2) {
+  console.log(i);
+}
+
