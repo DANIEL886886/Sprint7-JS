b7792fa5d41529803434bd15b995cbf890c45abf
DANIEL886886 <danielparaschiv886@gmail.com>
2024-05-13 20:14:05 +0300

    Ex2_Sprint7-JS_corectata

--- ex2/ex2.index.js
+++ ex2/ex2.index.js
@@ -7,15 +7,21 @@
 // `Input: [3.45, -2.68, 356, -75.96, 64, 19.28]`
 // `Output: 364.09000000000003`
 
-let numbers = [156, -1, -1, 34, -1, 12, 46, 2]
+let numbers = [1, -1, 1, 2, -2]
 let sum = 0
 let product = 1
 for (let i = 0; i < numbers.length; i++) {
        sum += numbers[i];
        product *= numbers[i];
 }
-let difference = sum - numbers[0]
+let difference1 = sum - numbers[0]
+
+let difference2 = - sum
+
+let difference3 = - numbers[0]
 
 console.log("Suma este:      ", sum);
-console.log("Diferenta este: ", difference);
+console.log("Diferenta1 este: ", difference1);
+console.log("Diferenta2 este: ", difference2);
+console.log("Diferenta3 este: ", difference3);
 console.log("Produsul este:  ", product);
